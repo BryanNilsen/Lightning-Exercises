b27c25fb2921f7c4492a95fc0f23970c4ddedd99
BryanNilsen <bryannilsen@gmail.com>
2019-07-18 10:32:03 -0500

    initial commit

---
+++ movieBadGuys/scripts/key.js
@@ -0,0 +1,71 @@
+const movieBadGuys = ["Johnny Lawrence", "The Joker", "Freddy Krueger", "Lord Voldemort", "Darth Vader", "Palpatine", "Bane", "Jigsaw", "Doctor Evil", "The Blob", "Cujo", "Jabba the Hutt", "Tyler Durden", "Ivan Drago", "Biff Tannen", "Professor Severus Snape"]
+
+// TODO: Iterate the above array of movie villains and for each character with names less than 12 characters, log their name to the console
+
+
+// TODO: Iterate the array of movie villains and for each character with names less than 12 characters AND contain more than one word, log their name to the console
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TODO: Iterate the array of movie villains and for each character with names greater than 8 characters AND DO NOT contain more than one word, log their name to the console
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// movieBadGuys.forEach(badGuy => {
+//   if (badGuy.length < 12) {
+//     console.log(badGuy)
+//   }
+// })
+
+
+
+// movieBadGuys.forEach(badGuy => {
+//   if (badGuy.length < 12 && badGuy.includes(" ")) {
+//     console.log(badGuy)
+//   }
+// })
+
+
+
+// movieBadGuys.forEach(badGuy => {
+//   if (badGuy.length > 8 && !badGuy.includes(" ")) {
+//     console.log(badGuy)
+//   }
+// })
---
+++ movieBadGuys/scripts/main.js
@@ -0,0 +1,5 @@
+console.log("SWEEP THE LEG!!");
+
+const movieBadGuys = ["Johnny Lawrence", "The Joker", "Freddy Krueger", "Lord Voldemort", "Darth Vader", "Palpatine", "Bane", "Jigsaw", "Maleficent", "Doctor Evil", "The Blob", "Cujo", "Jabba the Hutt", "Tyler Durden", "Ivan Drago", "Biff Tannen", "Professor Severus Snape", "The Fratellis", "Beetlejuice"];
+
+// TODO: Iterate the above array of movie villains and for each character with names less than 12 characters, log their name to the console
