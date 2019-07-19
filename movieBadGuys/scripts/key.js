0fafee5196346e25669f013cce29c5ffe59b8671
BryanNilsen <bryannilsen@gmail.com>
2019-07-19 12:03:52 -0500

    removed code from main.js and updated key.js

--- movieBadGuys/scripts/key.js
+++ movieBadGuys/scripts/key.js
@@ -1,69 +1,32 @@
-const movieBadGuys = ["Johnny Lawrence", "The Joker", "Freddy Krueger", "Lord Voldemort", "Darth Vader", "Palpatine", "Bane", "Jigsaw", "Doctor Evil", "The Blob", "Cujo", "Jabba the Hutt", "Ivan Drago", "Biff Tannen", "Professor Severus Snape", "Johnny Ringo"]
+const movieBadGuys = ["Johnny Lawrence", "The Joker", "Freddy Krueger", "Lord Voldemort", "Darth Vader", "Palpatine", "Bane", "Jigsaw", "Doctor Evil", "The Blob", "Cujo", "Jabba the Hutt", "Ivan Drago", "Biff Tannen", "Professor Severus Snape", "Johnny Ringo", "Beetlejuice"]
 
 // TODO: Iterate the above array of movie villains and for each character with names less than 12 characters, log their name to the console
 
+movieBadGuys.forEach(badGuy => {
+  if (badGuy.length < 12) {
+    console.log(badGuy)
+  }
+})
+
+// ? ALTERNATIVE METHOD (using 'filter' to push to new array and log)
+let badGuysWithNamesShorterThanTwelve = movieBadGuys.filter(badGuy => badGuy.length < 12)
+console.log('Baddies with short Names: ', badGuysWithNamesShorterThanTwelve);
 
 // TODO: Iterate the array of movie villains and for each character with names less than 12 characters AND contain more than one word, log their name to the console
 
+movieBadGuys.forEach(badGuy => {
+  if (badGuy.length < 12 && badGuy.includes(" ")) {
+    console.log(badGuy)
+  }
+})
 
 
+// TODO: Iterate the array of movie villains and for each character with names greater than 8 characters AND DO NOT contain more than one word, push their name into a new array and log that new array in the console.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// TODO: Iterate the array of movie villains and for each character with names greater than 8 characters AND DO NOT contain more than one word, log their name to the console
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// movieBadGuys.forEach(badGuy => {
-//   if (badGuy.length < 12) {
-//     console.log(badGuy)
-//   }
-// })
-
-
-// movieBadGuys.forEach(badGuy => {
-//   if (badGuy.length < 12 && badGuy.includes(" ")) {
-//     console.log(badGuy)
-//   }
-// })
-
-
-// movieBadGuys.forEach(badGuy => {
-//   if (badGuy.length > 8 && !badGuy.includes(" ")) {
-//     console.log(badGuy)
-//   }
-// })
+const badGuysWithOneNameLongerThanEightCharacters = []
+movieBadGuys.forEach(badGuy => {
+  if (badGuy.length > 8 && !badGuy.includes(" ")) {
+    badGuysWithAtLeastTwoNamesAndLongerThanEightCharacters.push(badGuy)
+  }
+})
+console.log(badGuysWithOneNameLongerThanEightCharacters)
--- movieBadGuys/scripts/main.js
+++ movieBadGuys/scripts/main.js
@@ -1,5 +1,12 @@
 console.log("SWEEP THE LEG!!");
 
-const movieBadGuys = ["Johnny Lawrence", "The Joker", "Freddy Krueger", "Lord Voldemort", "Darth Vader", "Palpatine", "Bane", "Jigsaw", "Doctor Evil", "The Blob", "Cujo", "Jabba the Hutt", "Ivan Drago", "Biff Tannen", "Professor Severus Snape", "Johnny Ringo"]
+const movieBadGuys = ["Johnny Lawrence", "The Joker", "Freddy Krueger", "Lord Voldemort", "Darth Vader", "Palpatine", "Bane", "Jigsaw", "Doctor Evil", "The Blob", "Cujo", "Jabba the Hutt", "Ivan Drago", "Biff Tannen", "Professor Severus Snape", "Johnny Ringo", "Beetlejuice"]
 
 // TODO: Iterate the above array of movie villains and for each character with names less than 12 characters, log their name to the console
+
+
+// TODO: Iterate the array of movie villains and for each character with names less than 12 characters AND contain more than one word, log their name to the console
+
+
+// TODO: Iterate the array of movie villains and for each character with names greater than 8 characters AND DO NOT contain more than one word, push their name into a new array and log that new array in the console.
+
